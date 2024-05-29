37ce594ac03d48303cfb26b4d37d9ac31007cdfa
Ryan Paolo Joson <bantora.1011@gmail.com>
2024-05-29 22:26:44 +0800

    test: render button component

---
+++ src/components/button/Button.test.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react'
+import Button from './Button';
+
+describe("button component", () => {
+	it('renders the button component', () => {
+		const ButtonComponent = render(<Button color='primary'>Button Component</Button>);
+
+		expect(ButtonComponent);
+	})
+});
