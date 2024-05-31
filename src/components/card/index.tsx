b422cf59fd1e90889c7000961a25fd7e4fcca81a
Ryan Paolo Joson <bantora.1011@gmail.com>
2024-05-31 23:10:10 +0800

    feat: simple card component and story

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,6 +1,16 @@
-import { ReactElement } from 'react';
+import { PropsWithChildren, ReactElement } from 'react';
 
-const Card = (): ReactElement => {
-	return <div></div>
+interface CardProps extends PropsWithChildren {
+	title?: string;
 }
-export default Card
+
+const Card = ({ title, children }: CardProps): ReactElement => {
+	return (
+		<div className='p-4 rounded-md border shadow-xl'>
+			<div className='text-xl font-bold text-blue-500'>{title}</div>
+			<div className='text-gray-500'>{children}</div>
+		</div>
+	);
+};
+
+export default Card;
